d938e91f0b4bca8f1116a302b897a04ca47b6767
UnchitthaP <unch.6672@gmail.com>
2022-08-31 11:23:06 +0700

    php3

---
+++ post.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><center>Webboard KakKak</center></h1>
+    <hr>
+    <center><?php echo "ต้องการดูกระทู้หมายเลข $_GET[id]" ; ?></center>
+    <center>
+    <table style="border:2px solid black; width:40%">
+        <tr bgcolor=" #6cd2fe">
+            <td align="left">แสดงความคิดเห็น</td>
+        </tr>
+        <tr><td><textarea rows="2" cols="69"></textarea><td></tr>
+        <tr><td><center><button>ส่งข้อความ</button></center></td></tr>
+    </table>
+    <a href="index.html">กลับไปหน้าหลัก</a>
+    </center>
+</body>
+</html>
